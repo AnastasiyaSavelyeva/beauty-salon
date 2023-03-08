d65d7598cf0373e0f1df5217a7a848dd2073d061
AnastasiyaSavelyeva <savelas22@yandex.ru>
2023-03-08 15:49:44 +0300

    work with links

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 (function () {
     'use strict';
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation');
 
-    // Loop over them and prevent submission
+    let forms = document.querySelectorAll('.needs-validation');
+
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
             form.addEventListener('submit', function (event) {
